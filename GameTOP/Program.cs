881f0aba10eccc0271b9ca44987b4c9443437443
mvpacheco29 <mvpacheco@hotmail.com>
2021-12-03 17:33:09 -0300

    primeiro commit

---
+++ GameTOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GameTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFODA("Ronaldo");
+
+            jogo.Iniciarjogo();
+        }
+    }
+
+    class JogoFODA
+    {
+        private readonly string _NomeJogador;
+
+        public JogoFODA(String nome)
+        {
+            this._NomeJogador = nome;
+        }
+
+        public void Iniciarjogo()
+        {   
+            Console.Write($"{_NomeJogador} deu um passe");
+        }
+    }
+}
